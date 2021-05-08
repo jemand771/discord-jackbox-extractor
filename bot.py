4180c829909d1abe9d0b5c2fb1311e155186bf73
jemand771 <jemand771@gmx.net>
2021-05-08 19:56:06 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,178 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import os
+import re
+import time
+from types import SimpleNamespace
+
+import discord
+from selenium.common.exceptions import TimeoutException
+
+from jackbox_scraper import ContentLoader
+
+client = discord.Client(max_messages=1000000)
+pattern = re.compile(r"^https?://games\.jackbox\.tv/artifact/.+/[0-9a-f]+/$")
+EMOJI = SimpleNamespace(**{
+    "RERUN": "🔄",
+    "DELETE": "🗑",
+    "LETTER": "💌"
+})
+TEXT_NUM_ARTIFACTS = "Number of artifacts"
+TEXT_NO_ARTIFACTS = "No artifacts found!"
+TEXT_NO_ARTIFACTS_VALUE = ":("
+TEXT_REQUESTER = "Requested by"
+TEXT_GAME_LINK = "Game id"
+
+
+async def asyncify(func):
+    return await asyncio.get_event_loop().run_in_executor(
+        ThreadPoolExecutor(), func)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Jackbox results"))
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+
+    if not pattern.match(message.content):
+        return
+    await load_and_send(message.content, message.channel, message.author.id)
+
+
+@client.event
+async def on_raw_reaction_add(payload: discord.raw_models.RawReactionActionEvent):
+
+    if payload.user_id == client.user.id:
+        return
+    channel: discord.TextChannel = await client.fetch_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    if message.author.id != client.user.id:
+        return
+    if not isinstance(payload.emoji, discord.PartialEmoji):
+        return
+    emoji: discord.PartialEmoji = payload.emoji
+    existing_reaction = [x for x in message.reactions if x.emoji == emoji.name]
+    if len(existing_reaction) != 1 or not existing_reaction[0].me:
+        return
+    # permission check
+    if emoji.name in (EMOJI.DELETE, EMOJI.RERUN) and not await check_message_perms(message, payload.user_id):
+        return
+    user = await client.fetch_user(payload.user_id)
+    # check if it's an embed message
+    if len(message.embeds) != 1:
+        return
+    embed = message.embeds[0]
+    if not embed.fields:
+        return
+    field_artifacts = [x for x in embed.fields if x.name in (TEXT_NUM_ARTIFACTS, TEXT_NO_ARTIFACTS)]
+    field_requester = [x for x in embed.fields if x.name == TEXT_REQUESTER]
+    field_link = [x for x in embed.fields if x.name == TEXT_GAME_LINK]
+    if any([len(x) != 1 for x in (field_artifacts, field_requester, field_link)]):
+        return
+    field_artifacts = field_artifacts[0]
+    field_requester = field_requester[0]
+    field_link = field_link[0]
+    num_artifacts = 0
+    if field_artifacts.value != TEXT_NO_ARTIFACTS_VALUE:
+        num_artifacts += int(field_artifacts.value)
+    if emoji.name == EMOJI.DELETE:
+        await delete_result(message, num_artifacts)
+    elif emoji.name == EMOJI.RERUN:
+        url = field_link.value.split("](")[1][:-1]
+        await delete_result(message, num_artifacts)
+        await load_and_send(url, message.channel, payload.user_id)
+    elif emoji.name == EMOJI.LETTER:
+        if user.dm_channel is None:
+            await user.create_dm()
+        await message.channel.send(f"{EMOJI.LETTER} hey <@{user.id}>, check your DMs!", reference=message, delete_after=10)
+        dm_msg = None
+        for msg in await get_result_messages(message, num_artifacts):
+            dm_msg = await user.dm_channel.send(content=msg.content, embed=msg.embeds[0] if msg.embeds else None)
+        if dm_msg:
+            await add_reaction_controls(dm_msg)
+    # TODO lock emoji
+    else:
+        print("unknown reaction")
+
+
+async def add_reaction_controls(message):
+    print("adding reactions")
+    for reaction in (EMOJI.RERUN, EMOJI.DELETE, EMOJI.LETTER):
+        await message.add_reaction(reaction)
+
+
+async def check_message_perms(message: discord.Message, user_id):
+    if isinstance(message.channel, discord.DMChannel):
+        return True
+    member = await message.channel.guild.fetch_member(user_id)
+    perms: discord.Permissions = member.permissions_in(message.channel)
+    return perms.manage_messages or perms.administrator
+
+
+async def load_and_send(url, channel, author_id):
+    start_time = time.time()
+    summary = discord.Embed()
+    summary.add_field(name=TEXT_NO_ARTIFACTS, value=TEXT_NO_ARTIFACTS_VALUE, inline=True)
+    summary.add_field(name=TEXT_REQUESTER, value=f"<@{author_id}>", inline=True)
+    summary.add_field(
+        name=TEXT_GAME_LINK,
+        value=f"[{url.split('/artifact/')[1][:-1]}]({url})",
+        inline=False)
+    # noinspection PyBroadException
+    wait_msg = await channel.send("downloading game data, hang on!")
+    try:
+        loader = await asyncify(lambda: ContentLoader(url))
+        # embeds = await asyncify(loader.get_messages)
+        await asyncify(loader.driver.quit)
+
+        embeds = [{"embed": discord.Embed(title="hello world", description="embed " + str(i))} for i in range(2)]
+        await wait_msg.delete()
+        for embed in embeds:
+            await channel.send(**embed)
+            await asyncio.sleep(0.5)
+        summary.set_field_at(0, name=TEXT_NUM_ARTIFACTS, value=str(len(embeds)), inline=True)
+        summary.title = "Success!"
+        summary.description = f"Detected a game of {loader.title}."
+        summary.set_thumbnail(url=loader.title_image)
+    except Exception as e:
+        await wait_msg.delete()
+        summary.title = "Error!"
+        summary.description = f"```\n{str(e)}```"
+        summary.set_thumbnail(url="https://www.freeiconspng.com/uploads/sign-red-error-icon-1.png")
+        raise
+
+        # return
+    finally:
+        summary.set_footer(text=f"Query time: {str(round(time.time() - start_time, 3))} seconds")
+        summary_msg = await channel.send(embed=summary)
+        print(type(summary_msg))
+        await add_reaction_controls(summary_msg)
+
+
+async def delete_result(message: discord.Message, num_messages):
+    for msg in await get_result_messages(message, num_messages):
+        await msg.delete()
+
+
+async def get_result_messages(message: discord.Message, num_messages):
+    for limit in range(100, 1000, 100):
+        messages = [x for x in await message.channel.history(limit=100, before=message).flatten() if
+                    x.author.id == client.user.id]
+        if len(messages) >= num_messages:
+            break
+    else:
+        # TODO messages not found -> error message?
+        return []
+    return messages[:num_messages] + [message]
+
+
+client.run(os.environ["DISCORD_TOKEN"])
+
+# https://s3.amazonaws.com/jbg-blobcast-artifacts/TeeKOGame/b0571f298d767fc9c8523d5696b303a9/anim_0.gif?junk=128
+# https://s3.amazonaws.com/jbg-blobcast-artifacts/TeeKOGame/b0571f298d767fc9c8523d5696b303a9/shirtimage-0.png
---
+++ jackbox_scraper.py
@@ -0,0 +1,156 @@
+import re
+import time
+
+import discord
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class GameLoader:
+    container_selector = ""
+    game_image = "https://static.thenounproject.com/png/140281-200.png"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def wait_for_gif(self, css_selector, container=None):
+        if container is None:
+            container = self.driver
+        WebDriverWait(container, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+        while "gif" not in container.find_element_by_css_selector(css_selector).get_attribute("src"):
+            time.sleep(0.01)
+
+    @staticmethod
+    def make_embed(name, png, gif, description=None):
+        description = description or discord.Embed.Empty
+        obj = discord.Embed(description=description, color=1)
+        obj.set_author(name=name, icon_url=png)
+        obj.set_image(url=gif)
+        return obj
+
+    def prepare_page(self):
+        return
+
+    def get_message(self, container):
+        return
+
+    def get_messages(self):
+        self.prepare_page()
+        return [{
+            "embed": self.get_message(x)
+        } for x in self.driver.find_elements_by_css_selector(self.container_selector)]
+
+
+class GameLoaderTeeKO(GameLoader):
+
+    container_selector = ".ui.fluid.container"
+    game_image = "https://jackboxgames.b-cdn.net/wp-content/uploads/2020/04/Tee-KO-No-Text.png"
+
+    def get_message(self, container):
+        name = container.find_element_by_css_selector(".shirt-title").text
+        description = None
+        if ":" in name:
+            description, name = name.split(": ")
+        png = container.find_element_by_css_selector("img").get_attribute("src")
+        gif = png.replace("shirtimage-", "anim_").replace(".png", ".gif")
+        # force gif generation
+        container.find_element_by_css_selector(".shirt-controls").find_elements_by_css_selector("button")[0].click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".close")))
+        self.wait_for_gif(".ui.segment img")
+        self.driver.find_element_by_css_selector(".close").click()
+        WebDriverWait(self.driver, 10).until_not(EC.presence_of_element_located((By.CSS_SELECTOR, ".close")))
+        return self.make_embed(name, png, gif, description)
+
+
+class GameLoaderQuiplash3(GameLoader):
+
+    container_selector = ".q3-matchup-artifact"
+    game_image = "https://i.pinimg.com/originals/8e/e6/63/8ee66306e9ab932ad6f75c1ac389bfb3.png"
+
+    def get_message(self, container):
+        container.find_element_by_css_selector(".q3-arrow:not(.open)").click()
+        img_sel = "img.image.q3-image"
+        png = container.find_element_by_css_selector(img_sel).get_attribute("src")
+        if ".png" not in png:
+            # TODO error image?
+            print("error getting static image for quiplash 3")
+        self.wait_for_gif(img_sel, container)
+        gif = container.find_element_by_css_selector(img_sel).get_attribute("src")
+        name = container.find_element_by_css_selector(".q3-question-title").text
+        return self.make_embed(name, png, gif)
+
+
+class GameLoaderSTI(GameLoader):
+    # TODO some parts of the embed don't load sometimes
+    container_selector = ".sti-burn-artifact"
+    game_image = "https://i.imgur.com/XtP9rgl.png"
+
+    def get_message(self, container):
+        container.find_element_by_css_selector(".sti-burn-arrow:not(.open)").click()
+        img_sel = "img.image.sti-image"
+        self.wait_for_gif(img_sel, container)
+        gif = container.find_element_by_css_selector(img_sel).get_attribute("src")
+        png = re.sub(r"anim(_\d+_\d+)\.gif.*", r"image\g<1>.png", gif)
+        name = container.find_element_by_css_selector(".sti-burn-title").text
+        description = container.find_element_by_xpath("../..").find_element_by_css_selector(".sti-round-title").text
+        return self.make_embed(name, png, gif, description)
+
+    def prepare_page(self):
+        # un-open first burn artifact
+        self.driver.find_element_by_css_selector(".sti-burn-arrow.open").click()
+        for game_round in self.driver.find_elements_by_css_selector(".sti-round-arrow:not(.open)"):
+            game_round.click()
+        ActionChains(self.driver).send_keys(Keys.HOME).perform()
+        time.sleep(0.5)
+
+
+class ContentLoader:
+
+    SPINNER_CLASS = ".loader.active"
+    GAME_CLASSES = {
+        "TeeKOGame": GameLoaderTeeKO,
+        "quiplash3Game": GameLoaderQuiplash3,
+        "STIGame": GameLoaderSTI
+        # TODO implement more games
+        # STI auto-replaces .png with .gif after a couple of seconds, 1_1 is always extended by default
+        #     maybe just click all down facing arrows?
+        # bracketeering also replaces png with gif, but not always? -> investigate further
+    }
+
+    def __init__(self, url):
+        self.url = url
+        self.title = None
+        self.title_image = None
+        self.game_type = url.split("/artifact/")[1].split("/")[0]
+        if self.game_type not in self.GAME_CLASSES:
+            raise Exception("unknown game type! maybe the bot doesn't support it yet?")
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.set_window_size(1920, 1080)
+        self.driver.implicitly_wait(5)
+
+        self.driver.get(url)
+        try:
+            WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.CSS_SELECTOR, self.SPINNER_CLASS)))
+        except TimeoutException:
+            print("warning: loading spinner didn't load, proceeding anyway")
+
+        WebDriverWait(self.driver, 60).until_not(EC.presence_of_element_located((By.CSS_SELECTOR, self.SPINNER_CLASS)))
+        if "404 Page Not Found" in self.driver.page_source or self.driver.current_url != url:
+            self.driver.quit()
+            raise Exception("invalid link")
+
+        self.title_image = self.GAME_CLASSES[self.game_type].game_image.replace("https://", "http://")
+        self.title = self.driver.title.split(" Gallery")[0]
+
+    def get_messages(self):
+        return self.GAME_CLASSES[self.game_type](self.driver).get_messages()
